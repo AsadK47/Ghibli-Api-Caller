2ec8bb0288cd15e55e6ae9381c626faa518ab8ce
Asad Khan <asad_khan47@hotmail.co.uk>
2019-03-05 16:25:08 +0000

    Stripped back to basics without tests to run npm fetch

--- src/ApiCaller.js
+++ src/ApiCaller.js
@@ -1,9 +1,16 @@
 'use strict';
 
-function ApiCaller() {
-
+if (typeof fetch !== 'function') {
+  global.fetch = require('node-fetch-polyfill');
+} else {
+  console.log('Fetch failed!')
 }
 
-ApiCaller.prototype.callApi = function () {
-  return 'X'
-};
+const url = 'https://ghibliapi.herokuapp.com/films';
+fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+    });
